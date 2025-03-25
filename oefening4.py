5b9ee79c1d175cf43a10c6695511028f634136b4
Beyzanur Aydin <beyzanur.aydin@leerling.athenea.be>
2025-03-25 17:09:12 +0100

    Niet helemaal kunnen maken

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,11 @@
-def countTargetPairs():
+def countTargetPairs(nums, target):
+     n = len(nums)
+
+     for i in range(nums):
+         if i =/ target or 0 <= i < n:
+            continue
+    for j in range(target):
+        if j  =/ nums or  0 <= j < n:
+            
+
+
